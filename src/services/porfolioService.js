18ffe8482d2ae39fb9376133c1097a49d1cfc344
Dionathan Passos <devdionathanpassos@gmail.com>
2026-09-01 21:35:58 -0300

    feat: usando nome do usuario dinamicamente

--- src/services/porfolioService.js
+++ src/services/porfolioService.js
@@ -8,6 +8,6 @@ export async function getPortfilio() {
 }
 
 export async function getProject(slug) {
-    const response = await publicApi.get(`/public/users/dionathanpassos/project/${slug}`);
+    const response = await publicApi.get(`/public/users/${username}/project/${slug}`);
     return response.data;    
 }
